4cb18694e580c34c76f391578d46a1d4e643d75e
Mutasem-Alhariri <alhari01@email.franklin.edu>
2023-02-19 22:25:22 -0500

    Revert "Revert "Merge pull request #32 from jabrad01/main""
    This reverts commit 032eb7a6294ad41ebc22c5cb1993e02489160d0f.

---
+++ Backend/PerfectShoes/BusinessLogic/EmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using PerfectShoes.Models;
+
+namespace PerfectShoes.BusinessLogic
+{
+    public class EmployeeService : Service, IEmployeeService
+    {
+        public EmployeeService(DataContext context) : base(context)
+        {
+        }
+
+        public Employee? AuthenticateAsEmployee(string email, string password)
+        {
+            return this._context.Employees.FirstOrDefault(employee => employee.Email == email && employee.Password == password);
+        }
+    }
+}
---
+++ Backend/PerfectShoes/BusinessLogic/IEmployeeService.cs
@@ -0,0 +1,9 @@
+﻿using PerfectShoes.Models;
+
+namespace PerfectShoes.BusinessLogic
+{
+    public interface IEmployeeService
+    {
+        public Employee? AuthenticateAsEmployee(string email, string password);
+    }
+}
