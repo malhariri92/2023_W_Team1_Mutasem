ec9e30f05e2b9c7e0ac31e720c1387fc90218ea2
Grandpa Seth <51179552+nnapolitano@users.noreply.github.com>
2023-03-04 21:13:58 -0500

    Update ProductDetails.vue
    added better formatting to productdetails page

--- Frontend/perfectshoes/src/components/ProductDetails.vue
+++ Frontend/perfectshoes/src/components/ProductDetails.vue
@@ -1,37 +1,36 @@
 <template>
   <div>
-    <div class = "productDetails">
-      <div class= "productName">
-        <h3>Product Name </h3>
-        {{ state.product.name }}
-      </div>
-      <div class= "productImage">
-        <h3>Product Image </h3>
-      {{ state.product.imageUrl }}
-      </div>
-      <div class= "productPrice">
-        <h3>Product Price </h3>
-        ${{ state.product.price }}
-      </div>
-      <div class= "productOptions" v-if="state.product.specs !== null && state.product.specs.length !== 0">
-        <h3>Product Options </h3>
-        The Options for this product are:
-        <div v-for="(attr, index) in state.product.specs" :key="index">
-          <p>Attribute name: {{ attr.name }} Attribute value: {{ attr.value }}</p>
+    <div class="productDetails">
+      <div class="productsDetails">
+        <div class="productImage">
+          <img :src=state.product.imageUrl :alt="state.product.description" />
+        </div>
+        <div class="productDetailsPanel">
+          <div class="productNameAndPrice">
+            <h4><b>{{ state.product.name }}</b></h4>
+            ${{ state.product.price }}
+          </div>
+          <div class="productDescription">
+            <h5>Description</h5>
+            {{ state.product.description }}
+          </div>
+          <div class="productOptions" v-if="state.product.specs !== null && state.product.specs.length !== 0">
+            <h5>Avalable Options: </h5>
+            <div v-for="(attr, index) in state.product.specs" :key="index">
+              <p>{{ attr.name }} - {{ attr.value }}</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div class= "productDescription">
-        <h3>Product Description </h3>
-      The Description is: {{ state.product.description }}
+      <div class="buttons">
+        <button class="btn btn-primary col-4 ms-1" @click="back">Back </button>
+        <button class="btn btn-success col-4 ms-1" @click="addToCart">Add to Cart </button>
+        <div style="display:none;" id="msg" class="alert alert-success mt-1" role="alert">
+          Product was added to your cart!
+        </div>
       </div>
-      <button class="btn btn-primary col-4 ms-1" @click="back">Back </button>
-      <button class="btn btn-success col-4 ms-1" @click="addToCart">Add to Cart </button>
-      <div style="display:none;" id="msg" class="alert alert-success mt-1" role="alert">
-        Product was added to your cart!
-      </div>
-    </div> 
-    
-</div> 
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -40,7 +39,7 @@ import { useRouter } from 'vue-router';
 import $ from 'jquery';
 
 const router = useRouter();
-const  store = inject('store');
+const store = inject('store');
 
 const state = reactive({
   product: store.currentProduct.value
@@ -50,26 +49,23 @@ function back() {
   router.push('/');
 }
 
-function addToCart()
-{
+function addToCart() {
   let isInCart = false;
-  if(store.cart.order.lineItems.length > 0)
-  {
-    for(let i = 0; i < store.cart.order.lineItems.length; i++)
-    {
-      if(store.cart.order.lineItems[i].productId === state.product.id) {
+  if (store.cart.order.lineItems.length > 0) {
+    for (let i = 0; i < store.cart.order.lineItems.length; i++) {
+      if (store.cart.order.lineItems[i].productId === state.product.id) {
         store.cart.order.lineItems[i].quantity++;
         isInCart = true;
         break;
       }
     }
   }
-  if(isInCart) {
+  if (isInCart) {
     store.methods.persistCart();
     $("#msg").show().delay(3000).fadeOut();
   }
   else {
-      const lineItem = 
+    const lineItem =
     {
       "unitPrice": state.product.price,
       "quantity": 1,
@@ -81,39 +77,76 @@ function addToCart()
     $("#msg").show().delay(3000).fadeOut();
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  div.products{
-    display:flex;
-    flex-wrap:wrap;
-    width: 100%;
-    justify-content:center ;
-    margin: 50px 0;
-  }
-  img{
-    width: 200px;
-    height: 200px;
-    text-align: center;
-    margin: 0 auto;
-    display:block;
-  }
-  .product{
-    width: 20%;
-    /* height: 5%; */
-    margin:15px;
-    box-sizing: border-box;
-    float:left;
-    border-radius:20px;
-    box-shadow: 0 14px 28px rgba(0,0,0,0.25);
-    background: #f2f2f2;
-    cursor: pointer;
-    
-  }
+div.productsDetails {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: center;
+  margin: 50px 0;
+}
 
-  .product:hover {
-    outline: .1rem solid blue
-  }
+div.productImage {
+  display: flex;
+  flex-wrap: wrap;
+  width: 50%;
+  justify-content: center;
+  margin: 50px 0;
+}
 
+div.productNameAndPrice {
+  flex-wrap: wrap;
+  width: 50%;
+  justify-content: center;
+  margin: 50px 0;
+}
+
+div.productOptions {
+  width: 50%;
+  justify-content: center;
+  margin: 50px 0;
+}
+
+div.productDescription {
+  flex-wrap: wrap;
+  width: 50%;
+  justify-content: center;
+  margin: 50px 0;
+}
+
+div.productDetailsPanel {
+  flex-wrap: wrap;
+  width: 50%;
+  justify-content: center;
+  margin: 50px 0;
+}
+
+img {
+  width: 300px;
+  height: 300px;
+  text-align: center;
+  margin: 0 auto;
+  display: block;
+}
+
+.product {
+  width: 20%;
+  /* height: 5%; */
+  margin: 15px;
+  box-sizing: border-box;
+  float: left;
+  border-radius: 20px;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25);
+  background: #f2f2f2;
+  cursor: pointer;
+
+}
+
+.product:hover {
+  outline: .1rem solid blue
+}
 </style>
