1b87d76141b200c99aa3438d9e319cf7ccf5b79a
GeoSchmidt <99700230+GeoSchmidt@users.noreply.github.com>
2023-03-10 14:52:39 -0500

    Updated the checkout page

---
+++ Backend/PerfectShoes/BusinessLogic/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using PerfectShoes.Models;
+using PerfectShoes.Models.DTO;
+
+
+namespace PerfectShoes.BusinessLogic
+{
+    public interface IOrderService
+    {
+        // Update to OrderDTO when the DTO is created
+        bool InsertOrder(Order order);
+    }
+}
---
+++ Backend/PerfectShoes/BusinessLogic/OrderService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PerfectShoes.BusinessLogic;
+using PerfectShoes.Models;
+using PerfectShoes.Models.DTO;
+
+namespace PerfectShoes.BusinessLogic
+{
+    public class OrderService : Service, IOrderService
+    {
+        public OrderService(DataContext context) : base(context) { }
+
+        // Update to use OrderDTO in future
+        public bool InsertOrder(Order order)
+        {
+            var ord = new Order
+            {
+                Customer = order.Customer,
+                Subtotal = order.Subtotal,
+                Tax = order.Tax,
+                Total= order.Total,
+                LineItems = order.LineItems
+            };
+            _context.Orders.Add(ord);
+            return _context.SaveChanges() > 0;
+        }
+    }
+}
